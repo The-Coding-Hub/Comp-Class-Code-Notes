909b71175f6f2c89e1fdb76ed1edef50a2403789
Prameya <yourcodinghub.py@gmail.com>
2021-10-18 12:08:33 +0530

    10-10-2021

---
+++ Python/arithmetic-opes.py
@@ -0,0 +1,38 @@
+# + Operator
+# To Add
+a = 10
+b = 11
+print(a+b)
+# To Concatenate
+c = "Hello"
+d = "World"
+print(c + ' ' + d)
+
+# - Operator
+e = 22
+f = 12
+print(e-f)
+
+# * Operator
+# To Multiply
+g = 10
+h = 5
+print(g*h)
+# To Repeat Strings
+i = "I am a good boy\n"
+print(i*3)
+
+# / Operator
+j = 15/3
+print(j)
+# // Operator
+k = 13//3
+print(k)
+
+# % Operator
+l = 12%5
+print(l)
+
+# ** Operator
+m = 3**4
+print(m)
